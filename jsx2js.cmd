2d29734844ac92b30d11c44a715782883c5ed335
deskach <dzianis.kachanau@gmail.com>
2015-12-05 09:52:15 +1000

    removed redundant files

--- jsx2js.cmd
+++
@@ -1,1 +0,0 @@
-C:\Users\%username%\AppData\Roaming\npm\jsx -x jsx "C:\Users\Dzianis\Documents\Visual Studio 2015\Projects\nineStars\nineStars\jsx" "C:\Users\Dzianis\Documents\Visual Studio 2015\Projects\nineStars\nineStars\js"
