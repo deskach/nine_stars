a69934235cfb40013d879eb365c17a8c540339be
deskach <dzianis.kachanau@gmail.com>
2015-12-05 10:38:13 +1000

    grunt works

---
+++ Gruntfile.js
@@ -0,0 +1,92 @@
+﻿module.exports = function (grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    processhtml: {
+      options: {
+        data: {
+          message: 'Hello world!'
+        }
+      },
+      dist: {
+        files: {
+          '../gh-pages/index.html': ['index.html']
+        }
+      }
+    },
+
+    cssmin: {
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+      dist: {
+        files: {
+          '../gh-pages/style/style-min.css': [
+            'style/style.css'
+          ]
+        }
+      }
+    },
+
+    //copy: {
+    //  main: {
+    //    files: [
+    //      {
+    //        expand: true,
+    //        flatten: true,
+    //        src: ['js/*'], dest: '../gh-pages/js/',
+    //        filter: 'isFile'
+    //      }
+    //    ],
+    //  },
+    //},
+
+    react: {
+      dynamic_mappings: {
+        files: [
+          {
+            expand: true,
+            cwd: 'jsx',
+            src: ['**/*.jsx'],
+            dest: 'js',
+            ext: '.js'
+          }
+        ]
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          '../gh-pages/js/script-min.js': ['js/script.js']
+        }
+      }
+    }
+
+    //browserify:     {
+    //  options:      {
+    //    transform:  [ require('grunt-react').browserify ]
+    //  },
+    //  app:          {
+    //    src:        'js/script.js',
+    //    dest:       'js/wibar.js'
+    //  }
+    //}
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  //grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-react');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-browserify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['react']);
+  grunt.registerTask('build', [
+    'react', 'cssmin', 'uglify', 'processhtml'
+  ]);
+};
